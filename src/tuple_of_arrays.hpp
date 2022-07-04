71fd192dce8dc8bf772e0fabef9b64fa4b9488aa
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-07-04 13:02:17 +0800

    Add tuple_of_arrays

---
+++ src/tuple_of_arrays.hpp
@@ -0,0 +1,130 @@
+#include <tuple>
+#include <type_traits>
+#include <vector>
+
+namespace detail {
+
+template<std::size_t N, typename ...Ts>
+struct parameter_pack_Nth {
+    static_assert(sizeof...(Ts) /* always false */, "parameter pack index out of range");
+};
+
+template<typename First, typename ...Rest>
+struct parameter_pack_Nth<0, First, Rest...> : std::type_identity<First> {
+};
+
+template<std::size_t N, typename First, typename ...Rest>
+struct parameter_pack_Nth<N, First, Rest...> : std::type_identity<typename parameter_pack_Nth<N - 1, Rest...>::type> {
+};
+
+}
+
+namespace detail {
+
+template<class TupleT, typename Func, typename ...Args, std::size_t ...I>
+constexpr auto tuple_for_each_impl(std::index_sequence<I...>, TupleT &t, Func, Args &&...args) {
+    (Func{}(get<I>(t), std::forward<Args>(args)...), ...); // TODO: what to return?
+}
+
+template<class TupleT, typename Func, typename ...Args>
+constexpr auto tuple_for_each(TupleT &t, Func /* [](auto &e, Args...) { ... } */, Args &&...args) {
+    tuple_for_each_impl(std::make_index_sequence<std::tuple_size_v<TupleT>>{}, t, Func{}, std::forward<Args>(args)...);
+}
+
+}
+
+namespace detail {
+
+template<class TupleT, typename Func, typename ...Elems, std::size_t ...I>
+constexpr auto tuple_transform_impl(std::index_sequence<I...>, TupleT &t, Func, Elems &&...arg2nd) {
+    (Func{}(get<I>(t), std::forward<Elems>(arg2nd)), ...);
+}
+
+template<class TupleT, typename Func, typename ...Elems>
+constexpr auto tuple_transform(TupleT &t, Func /* [](auto &e, auto &o) { ... } */, Elems &&...arg2nd) {
+    tuple_transform_impl(std::make_index_sequence<sizeof...(Elems)>{}, t, Func{}, std::forward<Elems>(arg2nd)...);
+}
+
+}
+
+namespace detail::force_adl {
+
+template<std::size_t N>
+constexpr auto &get(auto &arg) {
+    return get<N>(arg);
+}
+
+}
+
+template<typename ...Elems>
+class tuple_of_arrays {
+public:
+
+    template<typename ...Ts>
+    using tuple_template = std::tuple<Ts...>;
+
+    template<typename T>
+    using array_template = std::vector<T>;
+
+    using tuple_type = tuple_template<array_template<Elems>...>;
+
+    template<std::size_t I>
+    using Nth_value_type = typename detail::parameter_pack_Nth<I, Elems...>::type;
+
+    template<std::size_t I>
+    using Nth_array_type = array_template<typename detail::parameter_pack_Nth<I, Elems...>::type>;
+
+    using size_type = typename Nth_array_type<0>::size_type;
+
+    static constexpr std::size_t tuple_size = sizeof...(Elems);
+
+public:
+
+    tuple_of_arrays() = default;
+
+    explicit tuple_of_arrays(size_type num) : m_tuple({array_template<Elems>(num)...}) {
+    }
+
+    // TODO: other constructors, assign operators and swap
+
+    [[nodiscard]] constexpr size_type size() const noexcept { return get_array<0>().size(); }
+
+    constexpr void resize(size_type new_size) {
+        detail::tuple_for_each(m_tuple, [](auto &arr, size_type n) { arr.resize(n); }, new_size);
+    }
+
+    [[nodiscard]] constexpr bool empty() const noexcept { return get_array<0>().empty(); }
+
+    constexpr void reserve(size_type n) {
+        detail::tuple_for_each(m_tuple, [](auto &arr, size_type n) { arr.reserve(n); }, n);
+    }
+
+    template<std::size_t Array>
+    const Nth_array_type<Array> &get_array() const { return detail::force_adl::get<Array>(m_tuple); }
+
+    template<std::size_t Array>
+    [[nodiscard]] constexpr const Nth_value_type<Array> &get(size_type n) const { return get<Array>(m_tuple)[n]; }
+
+    template<std::size_t Array>
+    [[nodiscard]] constexpr Nth_value_type<Array> &get(size_type n) { return get<Array>(m_tuple)[n]; }
+
+    template<typename ...Args>
+    constexpr void push_back(Args &&...elems) {
+        static_assert(sizeof...(Args) == sizeof...(Elems));
+        detail::tuple_transform(
+                m_tuple,
+                [](auto &arr, auto &&e) { arr.push_back(std::forward<decltype(e)>(e)); },
+                std::forward<Args>(elems)...);
+    }
+
+    constexpr void pop_back() noexcept {
+        detail::tuple_for_each(m_tuple, [](auto &arr) { arr.pop_back(); });
+    }
+
+    constexpr void clear() noexcept {
+        detail::tuple_for_each(m_tuple, [](auto &arr) { arr.clear(); });
+    }
+
+private:
+    tuple_type m_tuple;
+};
