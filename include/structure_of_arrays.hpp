e73ce14febd30ac48a798436bc422f13684d44c2
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2023-02-25 11:56:41 +0800

    Use parameter pack instead of index sequence

--- include/structure_of_arrays.hpp
+++ include/structure_of_arrays.hpp
@@ -26,30 +26,6 @@ struct type_enumerate<std::index_sequence<Is...>, Ts...> {
 
 template <typename...> struct dummy {};
 
-template <typename MetaStruct, typename F, typename... Args, std::size_t... Is>
-inline constexpr void soa_for_each_impl(std::index_sequence<Is...>,
-                                        MetaStruct &&ms_self, F &&func,
-                                        Args &&...args) {
-  (std::invoke(std::forward<F>(func),
-               std::forward<MetaStruct>(ms_self).template get_array<Is>(),
-               std::forward<Args>(args)...),
-   ...);
-}
-
-// TODO: use Member parameter pack instead
-template <std::size_t N> struct soa_transform_helper;
-
-template <> struct soa_transform_helper<0> {
-  template <std::size_t... Is, typename S, typename F, typename... Args>
-  static constexpr auto apply(std::index_sequence<Is...>, S &&s_self, F &&f,
-                              Args &&...args) {
-    (std::invoke(std::forward<F>(f),
-                 std::forward<S>(s_self).template get_array<Is>(),
-                 std::forward<Args>(args)),
-     ...);
-  }
-};
-
 } // namespace detail
 
 template <typename MemberInfo> class structure_of_arrays_impl;
@@ -63,6 +39,35 @@ public:
   template <std::size_t N>
   using Nth_member_type = std::tuple_element_t<N, std::tuple<Members...>>;
 
+private:
+  template <typename M> constexpr decltype(auto) get_member() & {
+    return (static_cast<M &>(*this));
+  }
+
+  template <typename M> constexpr decltype(auto) get_member() const & {
+    return (static_cast<const M &>(*this));
+  }
+
+  template <typename M> constexpr decltype(auto) get_member() && {
+    return (static_cast<M &&>(*this));
+  }
+
+  template <typename M> constexpr decltype(auto) get_array() & {
+    return (static_cast<M &>(*this).value);
+  }
+
+  template <typename M> constexpr decltype(auto) get_array() const & {
+    return (static_cast<const M &>(*this).value);
+  }
+
+  template <typename M> constexpr decltype(auto) get_array() && {
+    return std::move(static_cast<M &&>(*this).value);
+  }
+
+#define SOA_ARRAY_PARAM_PACK                                                   \
+  std::forward<decltype(get_array<Members>())>(get_array<Members>())
+
+public:
   // TODO: find better solution in C++23
   template <std::size_t N> decltype(auto) get_array() & {
     return (static_cast<Nth_member_type<N> &>(*this).value);
@@ -81,56 +86,33 @@ public:
   constexpr auto empty() const { return size() == 0; }
 
   constexpr void resize(std::size_t new_size) {
-    detail::soa_for_each_impl(
-        std::make_index_sequence<narray>{}, *this,
-        [](auto &&array, std::size_t s) {
-          std::forward<decltype(array)>(array).resize(s);
-        },
-        new_size);
+    (SOA_ARRAY_PARAM_PACK.resize(new_size), ...);
   }
 
   constexpr void reserve(std::size_t new_size) {
-    detail::soa_for_each_impl(
-        std::make_index_sequence<narray>{}, *this,
-        [](auto &&array, std::size_t s) {
-          std::forward<decltype(array)>(array).reserve(s);
-        },
-        new_size);
+    (SOA_ARRAY_PARAM_PACK.reserve(new_size), ...);
   }
 
   constexpr void shrink_to_fit() {
-    detail::soa_for_each_impl(
-        std::make_index_sequence<narray>{}, *this, [](auto &&array) {
-          std::forward<decltype(array)>(array).shrink_to_fit();
-        });
+    (SOA_ARRAY_PARAM_PACK.shrink_to_fit(), ...);
   }
 
-  constexpr void clear() {
-    detail::soa_for_each_impl(
-        std::make_index_sequence<narray>{}, *this,
-        [](auto &&array) { std::forward<decltype(array)>(array).clear(); });
-  }
+  constexpr void clear() { (SOA_ARRAY_PARAM_PACK.clear(), ...); }
 
   constexpr void
   swap(structure_of_arrays_impl<detail::dummy<Members...>> &other) {
-    // FIXME
+    (SOA_ARRAY_PARAM_PACK.swap(other.get_array<Members>()), ...);
   }
 
   template <typename... Ts> constexpr void push_back(Ts &&...args) {
-    detail::soa_transform_helper<0>::apply(
-        std::make_index_sequence<narray>{}, *this,
-        [](auto &&arr, auto &&val) {
-          std::forward<decltype(arr)>(arr).push_back(
-              std::forward<decltype(val)>(val));
-        },
-        std::forward<Ts>(args)...);
+    (SOA_ARRAY_PARAM_PACK.push_back(std::forward<Ts>(args)), ...);
   }
 
   template <typename... Ts> constexpr void pop_back() {
-    detail::soa_for_each_impl(
-        std::make_index_sequence<narray>{}, *this,
-        [](auto &&array) { std::forward<decltype(array)>(array).pop_back(); });
+    (SOA_ARRAY_PARAM_PACK.pop_back(), ...);
   }
+
+#undef SOA_ARRAY_PARAM_PACK
 };
 
 template <template <typename> typename Array = std::vector, typename... T>
